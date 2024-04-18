51c9d87d5b9b95b61ed2d57781ab6a04d8e7c241
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2024-04-18 22:56:15 +0400

    comitment

--- Day81/classwork/a.py
+++ Day81/classwork/a.py
@@ -1,7 +1,32 @@
-def func(sia,vax):
-  sias= []
-  for i in sia:
-    if i != sia[vax]:
-      sias.append(i)
-  return sias
-print(func([0,1,5,6],-1))
+# def func(sia,vax):
+#   sias= []
+#   for i in sia:
+#     if i != sia[vax]:
+#       sias.append(i)
+#   return sias
+# print(func([0,1,5,6],-1))
+
+
+# person = {
+#     "name": "nika",
+#     "age" : 13
+# }
+# print(person["age"])
+# print(person["name"])
+
+
+username= input("name: ")
+passcode= input("passcode: ")
+
+data = {
+    "name": username,
+    "passcode" : passcode,
+}
+for i in data:
+    if len(data[i]) >= 15:
+        print("too much")
+    elif len(data[i]) <= 4:
+        print("too less")
+    else:
+        print(data)
+      
